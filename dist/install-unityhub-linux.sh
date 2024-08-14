34a5fe4791700361af3bcf95fc8e15f82cdd1b8b
Stephen Hodgson <hodgson.designs@gmail.com>
2024-08-14 14:23:06 -0400

    .

--- dist/install-unityhub-linux.sh
+++ dist/install-unityhub-linux.sh
@@ -4,7 +4,7 @@ echo "::group::Installing Unity Hub..."
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg >/dev/null
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends unityhub
+sudo apt-get install -y --no-install-recommends unityhub libgconf-2-4
 sudo apt-get clean
 hubPath=$(which unityhub)
 if [ -z "$hubPath" ]; then
