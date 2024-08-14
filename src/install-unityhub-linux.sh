91e0d22fd45a610a8729097001ab6cd19587474b
Stephen Hodgson <hodgson.designs@gmail.com>
2024-08-14 15:52:50 -0400

    .

--- src/install-unityhub-linux.sh
+++ src/install-unityhub-linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+dbus-uuidgen >/etc/machine-id && mkdir -p /var/lib/dbus/ && ln -sf /etc/machine-id /var/lib/dbus/machine-id
 echo "::group::Installing Unity Hub..."
 wget -qO - https://hub.unity3d.com/linux/keys/public | gpg --dearmor | sudo tee /usr/share/keyrings/Unity_Technologies_ApS.gpg >/dev/null
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/Unity_Technologies_ApS.gpg] https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
