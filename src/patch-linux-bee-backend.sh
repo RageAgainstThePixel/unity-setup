2d89818c2e9a8fdf35718b90b823f601d9266e1e
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-07-12 16:32:04 -0400

    cleanup

--- src/patch-linux-bee-backend.sh
+++ src/patch-linux-bee-backend.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
+set -e
 # https://discussions.unity.com/t/linux-editor-stuck-on-loading-because-of-bee-backend-w-workaround/854480
-
 UNITY_EDITOR_PATH="$1"
 UNITY_EDITOR_DATA_PATH="$(dirname "$UNITY_EDITOR_PATH")/Data"
-
 if [[ -f "$UNITY_EDITOR_DATA_PATH"/bee_backend && ! -f "$UNITY_EDITOR_DATA_PATH"/.bee_backend ]]; then
     mv "$UNITY_EDITOR_DATA_PATH"/{,.}bee_backend
     cat >"$UNITY_EDITOR_DATA_PATH"/bee_backend <<'EOF'
