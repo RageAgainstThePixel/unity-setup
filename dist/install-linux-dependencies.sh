d0daecbd92ed1a2ee125d57af71c3903260d5ade
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-09-16 17:32:51 -0400

    take arch into consideration when installing additional linux deps

--- dist/install-linux-dependencies.sh
+++ dist/install-linux-dependencies.sh
@@ -4,26 +4,22 @@
 set -e
 
 unityVersion="$1"
+arch="$(dpkg --print-architecture)"
 
 if [ -z "$unityVersion" ]; then
     echo "Usage: $0 <unity-version>"
     exit 1
 fi
 
-# Unity 2019.{1,2}
 case "$unityVersion" in
     2019.1.*|2019.2.*)
-    curl -LO https://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.0.0_1.0.2g-1ubuntu4.20_amd64.deb
-    sudo dpkg -i libssl1.0.0_1.0.2g-1ubuntu4.20_amd64.deb
-    rm libssl1.0.0_1.0.2g-1ubuntu4.20_amd64.deb
-    ;;
-esac
-
-# Unity 2019.{3,4}/2020.*
-case "$unityVersion" in
+        curl -LO "https://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.0.0_1.0.2g-1ubuntu4.20_${arch}.deb"
+        sudo dpkg -i "libssl1.0.0_1.0.2g-1ubuntu4.20_${arch}.deb"
+        rm "libssl1.0.0_1.0.2g-1ubuntu4.20_${arch}.deb"
+        ;;
     2019.3.*|2019.4.*|2020.*)
-    curl -LO https://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-    sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-    rm libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-    ;;
+        curl -LO "https://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_${arch}.deb"
+        sudo dpkg -i "libssl1.1_1.1.0g-2ubuntu4_${arch}.deb"
+        rm "libssl1.1_1.1.0g-2ubuntu4_${arch}.deb"
+        ;;
 esac
