a534491032285e0775199d0b947b2de3267799db
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-09-27 20:40:11 -0400

    update unity-cli

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { CheckAndroidSdkInstalled } from '@rage-against-the-pixel/unity-cli/dist/android-sdk';
+import { CheckAndroidSdkInstalled } from '@rage-against-the-pixel/unity-cli';
 import { ValidateInputs } from './inputs';
-import { UnityHub } from '@rage-against-the-pixel/unity-cli/dist/unity-hub';
+import { UnityHub } from '@rage-against-the-pixel/unity-cli';
 import core = require('@actions/core');
 
 const main = async () => {
